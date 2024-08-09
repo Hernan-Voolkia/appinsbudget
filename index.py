05b7e491b441a46737ab5cf14ea8b7b5deec45a2
Hernan-Voolkia <101428502+Hernan-Voolkia@users.noreply.github.com>
2024-08-09 09:16:09 -0300

    Favicon

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 bfHTML = """
 <html>
   <head>
-    <link rel="icon" type="image/x-icon" href="favicon.ico">
+    <link rel="icon" type="image/x-icon" href="/img/favicon.ico">
     <title>App</title>
   </head>
 <body>
