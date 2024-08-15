1018799b5e84666826d17092d568b9f91631204d
Hernan-Voolkia <101428502+Hernan-Voolkia@users.noreply.github.com>
2024-08-15 18:07:50 -0300

    Values 2024-09-15

---
+++ dbstatus.py
@@ -0,0 +1,23 @@
+bfHTMLdbCreateLog = """
+<html>
+  <head>
+    <link id="favicon" rel="icon" type="image/x-icon" href="/img/favicon.ico">
+    <title>App</title>
+  </head>
+<body>
+     <p>dbCreateLog finalizado</p>
+</body>
+</html>
+"""
+
+bfHTMLdbCreateAdminValue = """
+<html>
+  <head>
+    <link id="favicon" rel="icon" type="image/x-icon" href="/img/favicon.ico">
+    <title>App</title>
+  </head>
+<body>
+     <p><<value>></p>
+</body>
+</html>
+"""
