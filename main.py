a0c2c91d5714bea3d88b7036a7c07f9d99a31d68
Hernan-Voolkia <101428502+Hernan-Voolkia@users.noreply.github.com>
2024-08-15 23:47:26 -0300

    Values 2024-09-15 V20

--- main.py
+++ main.py
@@ -318,7 +318,7 @@ async def dbreadAdmin():
     engine = db.create_engine('postgresql://appinsbudgetuser:oGcfNsvSvdQsdmZGK6PnfsTGASpEg2da@dpg-cq3b65qju9rs739bbnb0-a/appinsbudgetdb')
     #engine = db.create_engine('sqlite:///appinsbudget.sqlite3')
     conn = engine.connect()
-    result = conn.execute(text('''SELECT * FROM admvalue;'''))
+    result = conn.execute(text('''SELECT stname, flvalue FROM admvalue;'''))
     for row in result:
         lsResult.append(row)    
     conn.close()
