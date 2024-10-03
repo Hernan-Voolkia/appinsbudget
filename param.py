dd0ed6d25a58bb445584cb916bdbc1a09a1a8927
Hernan-Voolkia <101428502+Hernan-Voolkia@users.noreply.github.com>
2024-10-03 19:38:24 -0300

    Perito

--- param.py
+++ param.py
@@ -1,6 +1,30 @@
+#Base
 bfAsegurado = 1
 bfTercero = 0.8
 bfMObra = 18250
 bfMOMinimo = 9150
 bfPintura = 22500
 bfAjuste = 1
+#Lateral
+bfLat_Cristal_Delantero = 1
+bfLat_Cristal_Trasero = 1
+bfLat_Espejo_Electrico = 1
+bfLat_Espejo_Manual = 1
+bfLat_Manija_Pta_Del = 1
+bfLat_Manija_Pta_Tras = 1
+bfLat_Moldura_Pta_Del = 1
+bfLat_Moldura_Pta_Tras = 1
+bfLat_Puerta_Delantera = 1
+bfLat_Puerta_Trasera = 1
+bfLat_Puerta_Panel_Del = 1
+bfLat_Puerta_Panel_Tras = 1
+bfLat_Zocalo = 1
+#Trasero
+bfBaul_Porton = 1
+bfFaro_Ext = 1
+bfFaro_Int = 1
+bfGuardabarro = 1
+bfLuneta = 1
+bfMoldura = 1
+bfPanel_Cola = 1
+bfParagolpe = 1
